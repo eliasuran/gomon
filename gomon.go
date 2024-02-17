d2b3a8ee92aaf657ca01bd0a42eec8273b276672
Elias Rissanen Uran <elura001@osloskolen.no>
2024-02-17 15:10:43 +0100

    ascii text added

--- gomon.go
+++ gomon.go
@@ -1,14 +1,60 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
+
+	"github.com/fatih/color"
 )
 
 func main() {
+	help := parseFlags()
+	if help {
+		fmt.Println("Provide the path to a directory containing a main.go file to run gomon")
+		return
+	}
+
+	file := getPath()
+
+	cmd := exec.Command("go", "run", file)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	fmt.Print(`
+ ██████╗  ██████╗ ███╗   ███╗ ██████╗ ███╗   ██╗
+██╔════╝ ██╔═══██╗████╗ ████║██╔═══██╗████╗  ██║
+██║  ███╗██║   ██║██╔████╔██║██║   ██║██╔██╗ ██║
+██║   ██║██║   ██║██║╚██╔╝██║██║   ██║██║╚██╗██║
+╚██████╔╝╚██████╔╝██║ ╚═╝ ██║╚██████╔╝██║ ╚████║
+ ╚═════╝  ╚═════╝ ╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+
+`)
+	color.HiMagenta("Made by eliasuran")
+	color.Green("[ 200 ] Starting program")
+
+	err := cmd.Run()
+	handleErr("Error executing command, ", err)
+
+}
+
+func handleErr(message string, err error) {
+	if err != nil {
+		fmt.Println(message, err)
+	}
+}
+
+func parseFlags() bool {
+	var helpFlag bool
+	flag.BoolVar(&helpFlag, "help", false, "help me")
+	flag.Parse()
+
+	return helpFlag
+}
+
+func getPath() string {
 	files := os.Args[1:]
-	fmt.Println(files)
 	if len(files) == 0 {
 		fmt.Println("No file provided")
 		os.Exit(1)
@@ -16,11 +62,6 @@ func main() {
 		fmt.Println("Too many files provided")
 		os.Exit(1)
 	}
-	cmd := exec.Command("go", "run", files[0])
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Run()
-	if err != nil {
-		fmt.Printf("Error executing command. %v", err)
-	}
+
+	return files[0]
 }
